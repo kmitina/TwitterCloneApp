446b1ddecdf2d67a2fc7ce17319196b5bf18fcf2
Kris Mitina <kristmitina@gmail.com>
2021-11-30 16:25:01 +0100

    Setup NavigationController

--- TwitterClone/Controller/MainTabController.swift
+++ TwitterClone/Controller/MainTabController.swift
@@ -12,27 +12,37 @@ class MainTabController: UITabBarController {
     // MARK: - Properties
     
     // MARK: - Lifecycle
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-       configureViewControllers()
+        configureViewControllers()
     }
     
     // MARK: - Helpers
     
     func configureViewControllers() {
-        let feed = FeedController()
-        feed.tabBarItem.image = UIImage(named: "home_unselected")
-        let explore = ExploreController()
-        explore.tabBarItem.image = UIImage(named: "search_unselected")
-        let notifications = NotificationsController()
-        notifications.tabBarItem.image = UIImage(named: "like_unselected")
-        let conversations = ConversationsController()
-        conversations.tabBarItem.image = UIImage(named: "home_unselected")
-
-        viewControllers = [feed, explore, notifications, conversations]
         
+        let feed = FeedController()
+        let nav1 = templateNavigationController(image: UIImage(named: "home_unselected"), rootViewController: feed)
+        
+        let explore = ExploreController()
+        let nav2 = templateNavigationController(image: UIImage(named: "search_unselected"), rootViewController: explore)
+    
+        let notifications = NotificationsController()
+        let nav3 = templateNavigationController(image: UIImage(named: "like_unselected"), rootViewController: notifications)
+        
+        let conversations = ConversationsController()
+        let nav4 = templateNavigationController(image: UIImage(named: "home_unselected"), rootViewController: conversations)
+        
+        viewControllers = [nav1, nav2, nav3, nav4]
+    }
+    
+    func templateNavigationController(image: UIImage?, rootViewController: UIViewController) -> UINavigationController {
+        let nav = UINavigationController(rootViewController: rootViewController)
+        nav.tabBarItem.image = image
+        nav.navigationBar.barTintColor = .white
+        return nav
     }
     
 }
