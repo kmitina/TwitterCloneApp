7991a22c17e7df7191bc9b1731afb8ff47f1ea0c
Kris Mitina <kristmitina@gmail.com>
2021-11-30 15:35:13 +0100

    Configure project

---
+++ TwitterClone/Controller/MainTabController.swift
@@ -0,0 +1,20 @@
+//
+//  MainTabController.swift
+//  TwitterClone
+//
+//  Created by DVKSH on 30.11.21.
+//
+
+import UIKit
+
+class MainTabController: UITabBarController {
+    
+    // MARK: - LIFECYCLE
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .systemPink
+    }
+    
+}
