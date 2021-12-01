b23e97bc61259e4f7d4077a0da60ba82ee3b15df
Kris Mitina <kristmitina@gmail.com>
2021-12-01 19:05:20 +0100

    Added Login UI

---
+++ TwitterClone/Controller/Authentication/RegistrationController.swift
@@ -0,0 +1,30 @@
+//
+//  RegistrationController.swift
+//  TwitterClone
+//
+//  Created by DVKSH on 1.12.21.
+//
+
+import Foundation
+
+import UIKit
+
+class RegistrationController: UIViewController {
+    // MARK: - Properties
+    
+    // MARK: - Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureUI()
+        
+        
+    }
+    
+    // MARK: - Helpers
+    
+    func configureUI() {
+        view.backgroundColor = .twitterBlue
+    }
+    
+}
