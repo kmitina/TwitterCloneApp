1c11718bb8516ef427a1d047e9d17fdc8b7f18af
Kris Mitina <kristmitina@gmail.com>
2021-12-09 12:32:39 +0100

    Fetch user data from Firebase

---
+++ TwitterClone/API/UserService.swift
@@ -0,0 +1,22 @@
+//
+//  UserService.swift
+//  TwitterClone
+//
+//  Created by DVKSH on 9.12.21.
+//
+
+import Firebase
+
+struct UserService {
+    static let shared = UserService()
+    
+    func fetchUser() {
+        guard let uid = Auth.auth().currentUser?.uid else { return }
+        
+        REF_USERS.child(uid).observeSingleEvent(of: .value) { snapshot in
+            guard let dictionary = snapshot.value as? [String: AnyObject] else { return }
+            guard let username = dictionary["username"] as? String else { return }
+            print("DEBUG: USername: \(username) ")
+        }
+    }
+}
