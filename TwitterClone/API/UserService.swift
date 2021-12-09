f80f980995437c642ce130601244bfa982391573
Kris Mitina <kristmitina@gmail.com>
2021-12-09 14:01:02 +0100

    Create custom user object (model)

--- TwitterClone/API/UserService.swift
+++ TwitterClone/API/UserService.swift
@@ -7,6 +7,8 @@
 
 import Firebase
 
+
+
 struct UserService {
     static let shared = UserService()
     
@@ -15,8 +17,12 @@ struct UserService {
         
         REF_USERS.child(uid).observeSingleEvent(of: .value) { snapshot in
             guard let dictionary = snapshot.value as? [String: AnyObject] else { return }
-            guard let username = dictionary["username"] as? String else { return }
-            print("DEBUG: USername: \(username) ")
+         
+            let user = User(uid: uid, dictionary: dictionary)
+            
+            print("DEBUG: Userma,e is \(user.username)")
+            print("DEBUG: Userma,e is \(user.fullname)")
+
         }
     }
 }
