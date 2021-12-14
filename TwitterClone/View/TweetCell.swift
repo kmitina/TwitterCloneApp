043db5dc8bbe274897c5077f643789ae5b8a7177
Kris Mitina <kristmitina@gmail.com>
2021-12-14 17:29:24 +0100

    Populate each cell with tweet

--- TwitterClone/View/TweetCell.swift
+++ TwitterClone/View/TweetCell.swift
@@ -11,6 +11,10 @@ class TweetCell: UICollectionViewCell {
     
     // MARK: - Properties
     
+    var tweet: Tweet? {
+        didSet { configure() }
+    }
+    
     private let profileImageView: UIImageView = {
         let iv = UIImageView()
         iv.contentMode = .scaleAspectFit
@@ -130,4 +134,10 @@ class TweetCell: UICollectionViewCell {
     
     // MARK: - Helpers
     
+    func configure() {
+        guard let tweet = tweet else { return }
+        
+        captionLabel.text = tweet.caption
+        
+    }
 }
