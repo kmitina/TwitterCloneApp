00bc821db37e6604e9459b047e0b116310268346
Kris Mitina <kristmitina@gmail.com>
2021-12-14 13:33:18 +0100

    Fetch tweets from Firebase & create tweet object, setup CollectionView

---
+++ TwitterClone/Model/Tweet.swift
@@ -0,0 +1,8 @@
+//
+//  Tweet.swift
+//  TwitterClone
+//
+//  Created by DVKSH on 13.12.21.
+//
+
+import Foundation
---
+++ TwitterClone/View/TweetCell.swift
@@ -0,0 +1,8 @@
+//
+//  TweetCell.swift
+//  TwitterClone
+//
+//  Created by DVKSH on 14.12.21.
+//
+
+import Foundation
