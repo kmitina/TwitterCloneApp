54d582dd013c8e3275dcf4493338e21652bf3921
Kris Mitina <kristmitina@gmail.com>
2021-12-17 16:24:29 +0100

    Tweet viewModel

---
+++ TwitterClone/ViewModel/TweetViewModel.swift
@@ -0,0 +1,30 @@
+//
+//  TweetViewModel.swift
+//  TwitterClone
+//
+//  Created by DVKSH on 16.12.21.
+//
+
+import UIKit
+
+struct TweetViewModel {
+    
+    let tweet: Tweet
+    let user: User
+    var profileImageUrl: URL? {
+        return user.profileImageUrl
+    }
+    
+    var userInfoText: NSAttributedString {
+        let title = NSMutableAttributedString(string: user.fullname, attributes: [.font: UIFont.boldSystemFont(ofSize: 14)])
+        title.append(NSAttributedString(string: " @\(user.username)", attributes: [.font: UIFont.systemFont(ofSize: 14),
+                                                                                   .foregroundColor: UIColor.lightGray]))
+        return title
+    }
+    
+    init(tweet: Tweet) {
+        self.tweet = tweet
+        self.user = tweet.user
+    }
+    
+}
