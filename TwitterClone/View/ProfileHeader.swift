d43f0df39920d1b75d0db1c866e428a80cc1703e
Kris Mitina <kristmitina@gmail.com>
2021-12-20 17:36:07 +0100

    Profile CollectionView setup

---
+++ TwitterClone/View/ProfileHeader.swift
@@ -0,0 +1,24 @@
+//
+//  ProfileHeader.swift
+//  TwitterClone
+//
+//  Created by DVKSH on 20.12.21.
+//
+
+import UIKit
+
+class ProfileHeader: UICollectionReusableView {
+    // MARK: - Properties
+    
+    // MARK: - Lifecycle
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        backgroundColor = .red
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
