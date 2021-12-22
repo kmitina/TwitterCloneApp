524b5f7a067e46e0766d82de8c0ae101c461861f
Kris Mitina <kristmitina@gmail.com>
2021-12-22 13:40:32 +0100

    Profile header filter bar

--- TwitterClone/View/ProfileHeader.swift
+++ TwitterClone/View/ProfileHeader.swift
@@ -10,6 +10,8 @@ import UIKit
 class ProfileHeader: UICollectionReusableView {
     // MARK: - Properties
     
+    private let filterBar = ProfileFilterView()
+    
     private lazy var containerView: UIView = {
         let view = UIView()
         view.backgroundColor = .twitterBlue
@@ -94,6 +96,8 @@ class ProfileHeader: UICollectionReusableView {
         userDetailsStack.spacing = 4
         addSubview(userDetailsStack)
         userDetailsStack.anchor(top: profileImageView.bottomAnchor, left: leftAnchor, right: rightAnchor, paddingTop: 8, paddingLeft: 12, paddingRight: 12)
+        addSubview(filterBar)
+        filterBar.anchor(left: leftAnchor, bottom: bottomAnchor, right: rightAnchor, height: 50)
         
     }
     
