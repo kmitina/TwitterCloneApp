cca061a62b017ae94487212b6504a728131c1162
Kris Mitina <kristmitina@gmail.com>
2022-01-11 15:56:11 +0100

    Populate tableView with notifications

--- TwitterClone/View/NotificationCell.swift
+++ TwitterClone/View/NotificationCell.swift
@@ -11,6 +11,12 @@ class NotificationCell: UITableViewCell {
     
     // MARK: - Properties
     
+    var notification: Notification? {
+        didSet {
+            configure()
+        }
+    }
+    
     private lazy var profileImageView: UIImageView = {
         let iv = UIImageView()
         iv.contentMode = .scaleAspectFit
@@ -54,5 +60,15 @@ class NotificationCell: UITableViewCell {
     
     @objc func handleProfileImageTapped() {
         
+    }
+    
+    // MARK: - Helpers
+    
+    func configure() {
+        guard let notification = notification else { return }
+        let viewModel = NotificationViewModel(notification: notification)
+        
+        profileImageView.sd_setImage(with: viewModel.profileImageUrl)
+        notificationLabel.attributedText = viewModel.notificationText
     }
 }
