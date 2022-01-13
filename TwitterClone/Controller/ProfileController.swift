587a057a4410b9e1a21d8b5acf03aeb98969dc50
Kris Mitina <kristmitina@gmail.com>
2022-01-13 16:26:09 +0100

    Setup data source for filters

--- TwitterClone/Controller/ProfileController.swift
+++ TwitterClone/Controller/ProfileController.swift
@@ -16,8 +16,22 @@ class ProfileController: UICollectionViewController {
     // MARK: - Properties
     private var user: User
     
-    private var tweets = [Tweet]() {
-        didSet { collectionView.reloadData() }
+    private var selectedFilter: ProfileFilterOptions = .tweets {
+        didSet {
+            collectionView.reloadData()
+        }
+    }
+    
+    private var tweets = [Tweet]()
+    private var likeTweets = [Tweet]()
+    private var replies = [Tweet]()
+    
+    private var currentDataSource: [Tweet] {
+        switch selectedFilter {
+        case .tweets: return tweets
+        case .replies: return replies
+        case .likes: return likeTweets
+        }
     }
     
     // MARK: - Lifecycle
@@ -49,6 +63,7 @@ class ProfileController: UICollectionViewController {
     func fetchTweets() {
         TweetService.shared.fetchTweets(forUser: user) { tweets in
             self.tweets = tweets
+            self.collectionView.reloadData()
         }
     }
     
@@ -82,12 +97,12 @@ class ProfileController: UICollectionViewController {
 
 extension ProfileController {
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return tweets.count
+        return currentDataSource.count
     }
     
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath) as! TweetCell
-        cell.tweet = tweets[indexPath.row]
+        cell.tweet = currentDataSource[indexPath.row]
         return cell
     }
 }
