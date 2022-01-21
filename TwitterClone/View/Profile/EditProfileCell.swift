7d62025f35a5abbc74d58948efbe98acde7658a8
Kris Mitina <kristmitina@gmail.com>
2022-01-21 15:34:56 +0100

    Data Model

---
+++ TwitterClone/View/Profile/EditProfileCell.swift
@@ -0,0 +1,25 @@
+//
+//  EditProfileCell.swift
+//  TwitterClone
+//
+//  Created by DVKSH on 21.01.22.
+//
+
+import UIKit
+
+class EditProfileCell: UITableViewCell {
+    // MARK: - Properties
+    // MARK: - Lifecycle
+
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        backgroundColor = .systemPurple
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Helpers
+
+}
---
+++ TwitterClone/ViewModel/EditProfileViewModel.swift
@@ -0,0 +1,22 @@
+//
+//  EditProfileViewModel.swift
+//  TwitterClone
+//
+//  Created by DVKSH on 21.01.22.
+//
+
+import Foundation
+
+enum EditProfileOptions: Int, CaseIterable {
+    case fullname
+    case username
+    case bio
+    
+    var description: String {
+        switch self {
+        case .username: return "Username"
+        case .fullname: return "Name"
+        case .bio: return "Bio"
+        }
+    }
+}
